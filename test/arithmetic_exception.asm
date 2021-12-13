c7de20c182b1bd693493335d62c637cb29c494dc
Darius Jankauskas <d5926j@gmail.com>
2021-12-13 03:20:09 -0500

    Improve test

--- test/arithmetic_exception.asm
+++ test/arithmetic_exception.asm
@@ -1,21 +1,19 @@
-li     $t0, 0xffffffff
+li     $t0, 0x7fffffff
 li     $t1, 2
+li     $t9, -3
 addu   $t2, $t0, $t1 
-subu   $t3, $t1, $t0 
-addiu  $t4, $t0, 2 
-subiu  $t5, $t0, -2
+subu   $t3, $t0, $t9 
+addiu  $t4, $t0, 4
 
-li     $ra, 36 
-add    $t6, $t0, $t1 
-li     $ra, 44 
-sub    $t7, $t1, $t0 
-li     $ra, 52 
-addi   $t8, $t0, 2 
-li     $ra, 60 
-subi   $t9, $t0, -2
+li     $ra, 32 
+add    $t5, $t0, $t1 
+li     $ra, 40 
+sub    $t6, $t1, $t0 
+li     $ra, 48 
+addi   $t7, $t0, 2 
 .word  0xfeedfeed
 
-.space 0x7fc4
+.space 0x7fd0
 
 jr     $ra
 addi   $s0, $s0, 1
