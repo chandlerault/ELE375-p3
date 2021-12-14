0029be7a2b5a841a4ccb0ebc6dbfbb849c80e870
DreamWeaver2k <chandlerault@gmail.com>
2021-12-14 15:29:38 -0500

    miss tests

--- test/miss.asm
+++ test/miss.asm
@@ -1,13 +1,13 @@
-# A simple test case that includes a load-use stall and a branch.
+# A simple test case to ensure that cache misses in direct mapped and hits in 2-way associative
 .set noreorder
 main:   addi    $t0, $zero, 4
         addi    $t1, $zero, 52
-        lw      $s1, 32($zero)     # s1 = AA
-        lw      $s1, 16($zero)     # s2 = AAAA
-        lw      $s1, 32($zero)     # s1 = AA
-        lw      $s1, 16($zero)     # s2 = AAAA
-        lw      $s1, 32($zero)     # s1 = AA
-        lw      $s1, 16($zero)     # s2 = AAAA
+        lw      $s1, 0($zero)     # s1 = AA
+        lw      $s2, 1024($zero)     # s2 = AAAA
+        lw      $s1, 0($zero)     # s1 = AA
+        lw      $s2, 1024($zero)     # s2 = AAAA
+        lw      $s1, 0($zero)     # s1 = AA
+        lw      $s2, 1024($zero)     # s2 = AAAA
         .word   0xfeedfeed  # pos 40
         .word   0x0         # pos 44
         .word   0x0         # pos 48
